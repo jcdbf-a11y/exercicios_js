14653047c2ee460283300ca95e0c0a943dc7dc90
jcdbf-a11y <jcdbf@aluno.ifnmg.edu.br>
2026-04-09 17:48:48 +0000

    feat: resolve exercicios da aula 05

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -7,25 +7,54 @@
 
 // Exercício 1 — classificarIdade
 export function classificarIdade(idade) {
-  // escreva seu código aqui
+  if (idade < 12 ){
+    return"criança";
+  }else if (idade < 18 ){
+    return"adolescente";
+  }else{
+    return"adulto";
+  }
+  // escreva seu código aquinpx
 }
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
+  return  preco - (preco / 100 * percentual)
   // escreva seu código aqui
 }
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
+  if(nota >= 60){
+    return "aprovado"
+  }else if (nota <60 && nota >= 40){
+    return "recuperação"
+  }else{
+    return "reprovado"
+  }
   // escreva seu código aqui
 }
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
+  let soma = 0
+  for ( const numero of numeros){
+    if(numero % 2 === 0 ){
+      soma+=numero
+    }
+  }
+  return soma
   // escreva seu código aqui
 }
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
+  let contador = 0
+  for(const caractere of texto){
+    if(caractere === letra){
+      contador++
+    }
+  }
+  return contador
   // escreva seu código aqui
 }
