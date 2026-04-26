dcaaa3d02c49bc69c8dd731354d5c476a56532a8
jcdbf-a11y <jcdbf@aluno.ifnmg.edu.br>
2026-04-26 16:50:26 +0000

    feat: resolve exercicios da aula 07

--- exercicios/aula07/index.js
+++ exercicios/aula07/index.js
@@ -7,16 +7,21 @@
 
 // Exercício 1 — objetoParaJSON
 export function objetoParaJSON(objeto) {
+  return JSON.stringify(objeto)
   // escreva seu código aqui
 }
 
 // Exercício 2 — jsonParaObjeto
 export function jsonParaObjeto(texto) {
+  return JSON.parse(texto)
   // escreva seu código aqui
 }
 
 // Exercício 3 — clonarObjeto
 export function clonarObjeto(objeto) {
+
+  const clone =  JSON.parse(JSON.stringify(objeto))
+  return clone
   // escreva seu código aqui
 }
 
@@ -34,10 +39,23 @@ function simularConsulta(id) {
 
 // Exercício 4 — buscarDados
 export async function buscarDados(id) {
+  const dados = await simularConsulta(id)
+  return dados
   // escreva seu código aqui
 }
 
 // Exercício 5 — dividirSeguro
 export async function dividirSeguro(a, b) {
+  try{
+    if(b == 0){
+      throw new Error('Divisão por zero')
+    }
+    else{
+      return a/b
+    } 
+     }catch (erro) {
+      throw erro
+    }
+  }
   // escreva seu código aqui
-}
+
